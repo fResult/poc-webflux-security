1f3223941492e320713740067010a066706a9154
fResult <styxmaz@gmail.com>
2025-07-09 02:48:18 +0700

    Create Functional Endpoint for `/fe/greetings` route.

--- src/main/kotlin/com/fResult/poc_webflux_security/GreetingRouters.kt
+++ src/main/kotlin/com/fResult/poc_webflux_security/GreetingRouters.kt
@@ -1,4 +1,23 @@
 package com.fResult.poc_webflux_security
 
-class GreetingRouters {
+import kotlinx.coroutines.reactive.awaitSingle
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.server.RouterFunction
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.coRouter
+
+@Configuration
+open class GreetingRouters {
+  @Bean
+  open fun greetingRouts(): RouterFunction<ServerResponse> = coRouter {
+    "/fe".nest {
+      GET("/greetings") { request ->
+        request.principal().map { it.name }
+          .map { GreetingResponse("Hello from Functional Endpoint, $it!") }
+          .flatMap { ServerResponse.ok().bodyValue(it) }
+          .awaitSingle()
+      }
+    }
+  }
 }
