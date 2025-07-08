7829d0af4ac95bb54023811845f4b92475cc6d48
fResult <styxmaz@gmail.com>
2025-07-09 02:26:14 +0700

    Create `securityFilterChain` bean in `SecurityConfiguration` class.

--- src/main/kotlin/com/fResult/poc_webflux_security/GreetingController.kt
+++ src/main/kotlin/com/fResult/poc_webflux_security/GreetingController.kt
@@ -1,4 +1,18 @@
 package com.fResult.poc_webflux_security
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
+
+data class GreetingResponse(val greetings: String)
+
+@RestController
+@RequestMapping("/greetings")
 class GreetingController {
+  @GetMapping
+  fun greet(@AuthenticationPrincipal user: Mono<UserDetails>) =
+    user.map(UserDetails::getUsername).map { GreetingResponse(it) }
 }
--- src/main/kotlin/com/fResult/poc_webflux_security/SecurityConfiguration.kt
+++ src/main/kotlin/com/fResult/poc_webflux_security/SecurityConfiguration.kt
@@ -2,8 +2,11 @@ package com.fResult.poc_webflux_security
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.Customizer
+import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService
 import org.springframework.security.core.userdetails.User
+import org.springframework.security.web.server.SecurityWebFilterChain
 
 @Configuration
 open class SecurityConfiguration {
@@ -22,6 +25,13 @@ open class SecurityConfiguration {
       .let { MapReactiveUserDetailsService(*it.toTypedArray()) }
   }
 
+  @Bean
+  open fun securityFilterChain(serverSecurity: ServerHttpSecurity): SecurityWebFilterChain =
+    serverSecurity.httpBasic(Customizer.withDefaults())
+      .authorizeExchange(::authorizeExchangeCustomizer)
+      .csrf(ServerHttpSecurity.CsrfSpec::disable)
+      .build()
+
   private val toUserBuilder: (Pair<String, String>) -> User.UserBuilder = { (username, password) ->
     User.withUsername(username).password("{noop}$password")
   }
@@ -31,4 +41,7 @@ open class SecurityConfiguration {
     1 -> userBuilder.roles("USER", "ADMIN")
     else -> userBuilder
   }
+
+  private fun authorizeExchangeCustomizer(exchangeSpec: ServerHttpSecurity.AuthorizeExchangeSpec) =
+    exchangeSpec.pathMatchers("/greetings").authenticated().anyExchange().permitAll()
 }
