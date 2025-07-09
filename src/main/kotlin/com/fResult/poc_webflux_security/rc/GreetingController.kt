cd99a7b643c4695dc1c00c36ae1f188dc0f23ade
fResult <styxmaz@gmail.com>
2025-07-10 00:45:40 +0700

    Create `/rc/greetings-with-roles` endpoint in Rest Controller.

--- src/main/kotlin/com/fResult/poc_webflux_security/rc/GreetingController.kt
+++ src/main/kotlin/com/fResult/poc_webflux_security/rc/GreetingController.kt
@@ -1,6 +1,7 @@
 package com.fResult.poc_webflux_security.rc
 
 import com.fResult.poc_webflux_security.GreetingResponse
+import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.web.bind.annotation.GetMapping
@@ -14,4 +15,9 @@ class GreetingController {
   @GetMapping("/greetings")
   fun greet(@AuthenticationPrincipal user: Mono<UserDetails>) =
     user.map(UserDetails::getUsername).map { GreetingResponse("Hello, from Rest Controller, $it") }
+
+  @GetMapping("/greetings-with-roles")
+  fun greetWithRoles(@AuthenticationPrincipal user: Mono<UserDetails>) =
+    user.map { it.authorities.map(GrantedAuthority::getAuthority) }
+      .map { GreetingResponse("Hello, from Rest Controller with roles, $it") }
 }
