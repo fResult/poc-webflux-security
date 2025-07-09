cc8919620bfcfa1a77dbd39b60572479ac1b4cd2
fResult <styxmaz@gmail.com>
2025-07-09 07:55:51 +0700

    Prepare `GreetingHandler#greetWithRoles`.

--- src/main/kotlin/com/fResult/poc_webflux_security/fe/GreetingHandler.kt
+++ src/main/kotlin/com/fResult/poc_webflux_security/fe/GreetingHandler.kt
@@ -2,6 +2,8 @@ package com.fResult.poc_webflux_security.fe
 
 import com.fResult.poc_webflux_security.GreetingResponse
 import kotlinx.coroutines.reactive.awaitSingle
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.GrantedAuthority
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
@@ -14,4 +16,15 @@ class GreetingHandler {
       .map { GreetingResponse("Hello from Functional Endpoint, $it!") }
       .flatMap { ServerResponse.ok().bodyValue(it) }
       .awaitSingle()
+
+  suspend fun greetWithRoles(request: ServerRequest): ServerResponse =
+    request.principal().cast(Authentication::class.java)
+      .map(::toAuthorities)
+      .map { GreetingResponse("Hello from Functional Endpoint with roles, $it!") }
+      .flatMap { ServerResponse.ok().bodyValue(it) }
+      .awaitSingle()
+
+  private fun toAuthorities(auth: Authentication): List<String> {
+    return auth.authorities.map(GrantedAuthority::getAuthority)
+  }
 }
